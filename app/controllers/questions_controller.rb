ba384206e3413bc4c3d3d6708e41604ef931c9e6
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-09 14:00:14 +0530

    test commit 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -126,7 +126,7 @@ class QuestionsController < ApplicationController
     unless FileTest.exists?("#{RAILS_ROOT}/public/temp_test/"+name+'.zip')
       Question.download(name)
       logger.info "*" * 80
-      looger.info "#{RAILS_ROOT}/public/temp_test/" + name + '.zip'
+      logger.info "#{RAILS_ROOT}/public/temp_test/" + name + '.zip'
       logger.info File.exits?("#{RAILS_ROOT}/public/temp_test/" + name + '.zip')
       logger.info "*" * 80
       send_file "#{RAILS_ROOT}/public/temp_test/" + name + '.zip', :type => "application/zip"
