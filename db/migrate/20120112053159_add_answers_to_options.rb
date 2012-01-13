b21bb2ef8694717a34e452a6536b0f77af3b6538
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-13 15:19:07 +0530

    test commit

--- db/migrate/20120112053159_add_answers_to_options.rb
+++ db/migrate/20120112053159_add_answers_to_options.rb
@@ -6,18 +6,20 @@ class AddAnswersToOptions < ActiveRecord::Migration
     Answer.reset_column_information
     
     Answer.all.each do |ans|
-#      opt = Option.where("question_id = ? and body = ?", ans.question_id, ans.body)
-      opt.update_all(:answer => true, ["question_id = ? and body = ?", ans.question_id, ans.body])
-#      execute "UPDATE options SET answer = true where question_id = #{ans.question_id} and body = \"#{ans.body}\""
+#      opt.update_all(:answer => true, ["question_id = ? and body = ?", ans.question_id, ans.body])
+
+      execute "UPDATE options SET answer = true where question_id = #{ans.question_id} and body = \"#{ans.body}\""
     end
     
-    answers = Answer.joins(:question).where("ques_type = 'Subjective'")
+#    answers = Answer.joins(:question).where("ques_type = 'Subjective'")
+
 #    answers = Answer.find(:all, :joins => "join questions on questions.id = answers.question_id and questions.ques_type = 'Subjective'", :include => :question)
-    answers.each do |ans|
-      Option.create!(:question_id => ans.question_id, :body => ans.body, :answer => true)
-    end
+
+#    answers.each do |ans|
+#      Option.create!(:question_id => ans.question_id, :body => ans.body, :answer => true)
+#    end
     
-#    execute "INSERT INTO options(question_id, body, answer, created_at, updated_at) SELECT answers.question_id, answers.body, true, answers.created_at, answers.updated_at FROM answers, questions WHERE answers.question_id = questions.id AND questions.ques_type = 'Subjective'"    
+    execute "INSERT INTO options(question_id, body, answer, created_at, updated_at) SELECT answers.question_id, answers.body, true, answers.created_at, answers.updated_at FROM answers, questions WHERE answers.question_id = questions.id AND questions.ques_type = 'Subjective'"    
   end
 
   def self.down
