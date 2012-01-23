a013853f7b4093dafa1b6b09fe271e33a6376e4d
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-23 13:51:54 +0530

    make_test pagination done

--- app/models/question.rb
+++ app/models/question.rb
@@ -113,38 +113,6 @@ class Question < ActiveRecord::Base
     end
   end
   
-  def self.count_questions_by_level(questions, level)
-    beg_temp, int_temp, mast_temp, beg, int, mast = 0,0,0,0,0,0
-    questions_temp = []
-    level.each do |index, val|
-      unless val.empty?
-        if index == "0"
-          beg = val.to_i
-        elsif index == "1"
-          int = val.to_i
-        elsif index == "2"
-          mast = val.to_i
-        end
-      end
-    end
-    unless questions.empty?
-      questions = questions.shuffle.shuffle
-      questions.each do |question|
-        if question.level.to_i == 0 && beg_temp < beg
-          questions_temp.push(question)
-          beg_temp += 1
-        elsif question.level.to_i == 1 && int_temp < int
-          questions_temp.push(question)
-          int_temp += 1
-        elsif question.level.to_i == 2 && mast_temp < mast
-          questions_temp.push(question)
-          mast_temp += 1
-        end
-      end
-    end
-    return questions_temp, beg_temp, int_temp, mast_temp
-  end
-  
   private
   
   ## Please DO NOT REPEAT - comparison with true
