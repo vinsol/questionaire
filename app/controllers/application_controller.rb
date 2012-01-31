9f11d33c77449b73626c83582e470134c2944f81
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-31 15:33:08 +0530

    current_admin method added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  helper_method :current_admin
+  
   def after_sign_out_path_for(resource_or_scope)
     session[:admin_id] = nil
     new_admin_session_path
+  end
+  
+  def current_admin
+    @admin ||= Admin.where(:id => session[:admin_id])
   end
   
   private
