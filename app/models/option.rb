5f907858640b54789f85cd2f777bdf3e715fa25d
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-31 15:22:41 +0530

    pluralize used

--- app/models/option.rb
+++ app/models/option.rb
@@ -3,5 +3,5 @@ class Option < ActiveRecord::Base
 
   belongs_to :question
   
-  validates :body, :presence => true
+  validates :body, :presence => true, :uniqueness => { :scope => :question_id }
 end
