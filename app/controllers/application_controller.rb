9f24df7ad241a2dfcd20fd61eeda9168435ee1ec
Priyank Gupta <priyank.gupta@vinsol.com>
2012-01-31 15:52:31 +0530

    seed file changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :current_admin
+  helper_method :current_admin, :admin_signed_in
   
   def after_sign_out_path_for(resource_or_scope)
     session[:admin_id] = nil
     new_admin_session_path
   end
   
+  protected
+  
   def current_admin
-    @admin ||= Admin.where(:id => session[:admin_id])
-  end
-  
-  private
-  
+    @admin ||= Admin.where(:id => session[:admin_id]).first
+  end  
+    
   def admin_signed_in
     unless session["devise.googleapps_data"]
       session[:admin_id] = nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+admin1 = Admin.find_or_initialize_by_email("shreya@vinsol.com")
+admin1.save!
+admin2 = Admin.find_or_initialize_by_email("priyank.gupta@vinso.com")
+admin2.save!
 
-admin = Admin.create!([{:email => "shreya@vinsol.com"}, {:email => "priyank.gupta@vinsol.com"}])
-
-category = Category.create!([{:name => "Web"}, {:name => "IOS"}, {:name => "Android"}, {:name => "Aptitude"}])
-
-
+category1 = Category.find_or_initialize_by_name("Web")
+category1.save!
+category2 = Category.find_or_initialize_by_name("IOS")
+category2.save!
+category3 = Category.find_or_initialize_by_name("Android")
+category3.save!
+category4 = Category.find_or_initialize_by_name("Aptitude")
+category4.save!
